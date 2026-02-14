a3be0225f2c02915565d152f6d9d73783c8ff5b5
infofrogames-hub <info.frogames@gmail.com>
2026-02-14 09:13:42 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,11 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// Se NON vuoi un file CSS, puoi commentare questa riga.
-// Se lo vuoi, crea "index.css" nella stessa cartella di questo file.
-import "./index.css";
-
 const rootElement = document.getElementById("root");
+
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
